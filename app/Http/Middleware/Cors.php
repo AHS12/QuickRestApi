fbe573bfd1d0b6612e0b47cc7b0f10b5f65c3956
Md Azizul Hakim <ahslucky12@gmail.com>
2020-08-07 19:45:07 +0600

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -32,3 +32,29 @@ class Cors
         return $next($request);
     }
 }
+
+
+//another solution
+// namespace App\Http\Middleware;
+
+// use Closure;
+// use Illuminate\Http\Request;
+
+// class Cors
+// {
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  Request  $request
+//      * @param  Closure  $next
+//      * @return mixed
+//      */
+//     public function handle($request, Closure $next)
+//     {
+//         return $next($request)
+//             ->header('Access-Control-Allow-Origin', '*')
+//             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//             ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+//             ->header('Access-Control-Allow-Credentials',' true');
+//     }
+// }
